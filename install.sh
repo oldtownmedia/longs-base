87ee89186071a95e9fd3658c3fbcef2724294a13
Mike Selander <mikeselander@gmail.com>
2015-12-06 19:52:29 -0700

    Making the install script a little more interesting, adding index.php

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo -e "\e[1mWP Build Script";
+echo "  "
+echo "|-------------------------|"
+echo "|     \033[1mWP Build Script\033[0m     |";
+echo "|-------------------------|"
+echo "  "
 
-echo -e "\e[96mGit repo URL to write to:"
+echo "\033[33mGit repo URL to write to:\033[0m"
 read git_repo
 export git_repo
 
-echo -e "\e[96mNew theme path:";
+echo "\033[33mNew theme path:\033[0m";
 read theme_path
 export theme_path
 
 # Delete reference to submodules that now need to be included as repo files
 git rm --cached content/mu-plugins
 git rm --cached content/themes
-echo -e "\e[91mdelete reference to submodule HEAD"
+echo "\033[91mdelete reference to submodule HEAD\033[0m"
 
 rm -rf .gitmodules
 mv .gitmodules-dev .gitmodules
@@ -21,19 +25,19 @@ echo "Replacing original gitmodules"
 
 rm -rf content/mu-plugins/.git
 rm -rf content/themes/skeleton/.git
-echo -e "\e[91mDeleting git files in prior submodules"
+echo "\033[91mDeleting git files in prior submodules\033[0m"
 
 # Rename the theme folder
 mv content/themes/skeleton content/themes/$theme_path
-echo -e "\e[32mRenaming the theme folder to $theme_path";
+echo "\033[32mRenaming the theme folder to $theme_path";
 
 # Remove old
 git remote rm origin
 
 # Re-add git to new repo
 git remote add origin $git_repo
-echo -e "\e[32mReplacing git origin with new path"
+echo "\033[32mReplacing git origin with new path"
 
 git add .
 git commit -am "Initial Commit"
-echo -e "\e[32mAdding all the new files to git for you for you"
+echo "\033[32mAdding all the new files to git for you for you"
